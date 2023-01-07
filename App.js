2253911220532eaa1013cb3ffb7928fe6a2ff9d0
corea2244 <59365194+corea2244@users.noreply.github.com>
2023-01-07 21:16:07 +0900

    [ch1 - 3 - UI 개발 (1) 헤더, SafeAreaView와 react-native-iphone-x-helper, …

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import {
+  getStatusBarHeight,
+  getBottomSpace,
+} from "react-native-iphone-x-helper";
+import Header from "./src/Header";
+
+const statusBarHeight = getStatusBarHeight();
+const bottomSpace = getBottomSpace();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
     </View>
   );
 }
@@ -14,7 +21,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: statusBarHeight,
   },
 });
---
+++ src/Header.js
@@ -0,0 +1,33 @@
+import { View, Text } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+const IconButton = (props) => {
+  return (
+    <View style={{ paddingHorizontal: 6 }}>
+      <Ionicons name={props.name} size={24} color="black" />
+    </View>
+  );
+};
+
+export default () => {
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        paddingVertical: 10,
+      }}
+    >
+      <Text style={{ fontSize: 32, fontWeight: "bold", marginLeft: 10 }}>
+        친구
+      </Text>
+
+      <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <IconButton name="search-outline" />
+        <IconButton name="person-add-outline" />
+        <IconButton name="md-musical-notes-outline" />
+        <IconButton name="ios-settings-outline" />
+      </View>
+    </View>
+  );
+};
