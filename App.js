b2faba691cfb2dfc8a279a155a623780eaacf658
corea2244 <59365194+corea2244@users.noreply.github.com>
2023-01-07 22:06:39 +0900

    [ch1 - 4 - UI 개발 (2) 내 프로필] 최종 코드

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
-import {
-  getStatusBarHeight,
-  getBottomSpace,
-} from "react-native-iphone-x-helper";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import { myProfile } from "./src/data";
 import Header from "./src/Header";
+import Margin from "./src/Margin";
+import MyProfile from "./src/MyProfile";
 
-const statusBarHeight = getStatusBarHeight();
-const bottomSpace = getBottomSpace();
+const statusBarHeight = getStatusBarHeight(true);
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
+
+      <Margin height={10} />
+
+      <MyProfile
+        uri={myProfile.uri}
+        name={myProfile.name}
+        introduction={myProfile.introduction}
+      />
     </View>
   );
 }
