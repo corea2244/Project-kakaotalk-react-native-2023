4554387548e9ac0f41ad7797a532dc5ca998b09c
corea2244 <59365194+corea2244@users.noreply.github.com>
2023-01-08 10:08:59 +0900

    [ch1 - 7 - UI 개발 (4) 탭바] 최종 코드

--- src/FriendList.js
+++ src/FriendList.js
@@ -1,17 +1,11 @@
 import { ScrollView, View } from "react-native";
-import { getBottomSpace } from "react-native-iphone-x-helper";
 import Margin from "./Margin";
 import Profile from "./Profile";
-
-const bottomSpace = getBottomSpace();
 
 export default (props) => {
   if (!props.isOpened) return null;
   return (
-    <ScrollView
-      showsVerticalScrollIndicator={false}
-      contentContainerStyle={{ paddingBottom: bottomSpace }}
-    >
+    <ScrollView showsVerticalScrollIndicator={false}>
       {props.data.map((item, index) => (
         <View key={index}>
           <Profile
